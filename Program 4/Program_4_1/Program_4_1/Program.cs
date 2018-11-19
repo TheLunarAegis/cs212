2d2f06cda551a6adf6bbd94db591ec86b613f3ce
TheLunarAegis <43121987+TheLunarAegis@users.noreply.github.com>
2018-11-19 18:22:02 -0500

    added program_04

---
+++ Program 4/Program_4_1/Program_4_1/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Bingo
+{
+    class Program
+    {
+        private static RelationshipGraph rg;
+
+        // Read RelationshipGraph whose filename is passed in as a parameter.
+        // Build a RelationshipGraph in RelationshipGraph rg
+        private static void ReadRelationshipGraph(string filename)
+        {
+            rg = new RelationshipGraph();                           // create a new RelationshipGraph object
+
+            string name = "";                                       // name of person currently being read
+            int numPeople = 0;
+            string[] values;
+            Console.Write("Reading file " + filename + "\n");
+            try
+            {
+                string input = System.IO.File.ReadAllText(filename);// read file
+                input = input.Replace("\r", ";");                   // get rid of nasty carriage returns 
+                input = input.Replace("\n", ";");                   // get rid of nasty new lines
+                string[] inputItems = Regex.Split(input, @";\s*");  // parse out the relationships (separated by ;)
+                foreach (string item in inputItems) 
+		{
+                    if (item.Length > 2)                            // don't bother with empty relationships
+                    {
+                        values = Regex.Split(item, @"\s*:\s*");     // parse out relationship:name
+                        if (values[0] == "name")                    // name:[personname] indicates start of new person
+                        {
+                            name = values[1];                       // remember name for future relationships
+                            rg.AddNode(name);                       // create the node
+                            numPeople++;
+                        }
+                        else
+                        {               
+                            rg.AddEdge(name, values[1], values[0]); // add relationship (name1, name2, relationship)
+
+                            // handle symmetric relationships -- add the other way
+                            if (values[0] == "hasSpouse" || values[0] == "hasFriend")
+                                rg.AddEdge(values[1], name, values[0]);
+
+                            // for parent relationships add child as well
+                            else if (values[0] == "hasParent")
+                                rg.AddEdge(values[1], name, "hasChild");
+                            else if (values[0] == "hasChild")
+                                rg.AddEdge(values[1], name, "hasParent");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write("Unable to read file {0}: {1}\n", filename, e.ToString());
+            }
+            Console.WriteLine(numPeople + " people read");
+        }
+
+        // Show the relationships a person is involved in
+        private static void ShowPerson(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            if (n != null)
+                Console.Write(n.ToString());
+            else
+                Console.WriteLine("{0} not found", name);
+        }
+
+        // Show a person's friends
+        private static void ShowFriends(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            if (n != null)
+            {
+                Console.Write("{0}'s friends: ",name);
+                List<GraphEdge> friendEdges = n.GetEdges("hasFriend");
+                foreach (GraphEdge e in friendEdges) {
+                    Console.Write("{0} ",e.To());
+                }
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine("{0} not found", name);     
+        }
+
+        //Show all orphans in the file
+        private static void ShowOrphans()
+        {
+
+            Console.Write
+                ("\n\n--------------------------------------------\n" +
+                "These are the potential identities of Batman" +
+                "\n--------------------------------------------\n\n");
+            foreach (GraphNode person in rg.nodes)
+            {
+                if (person != null)
+                {
+                    List<GraphEdge> parentEdges = person.GetEdges("hasParent");
+                    if(parentEdges.Count == 0)
+                    {
+                        Console.Write("{0} " + "\n", person);
+                    }
+                }
+            }
+            Console.Write
+                ("------------------------------------------------------\n" +
+                "One of these people meet the requirements to be Batman" +
+                "\n------------------------------------------------------\n\n");
+            Console.WriteLine();
+        }
+
+
+        // accept, parse, and execute user commands
+        private static void CommandLoop()
+        {
+            string command = "";
+            string[] commandWords;
+            Console.Write("Welcome to Bruce Wayne's Dutch Bingo Parlor!\n");
+
+            while (command != "exit")
+            {
+                Console.Write("\nEnter a command: ");
+                command = Console.ReadLine();
+                commandWords = Regex.Split(command, @"\s+");        // split input into array of words
+                command = commandWords[0];
+
+                if (command == "exit")
+                    ;                                               // do nothing
+
+                // read a relationship graph from a file
+                else if (command == "read" && commandWords.Length > 1)
+                    ReadRelationshipGraph(commandWords[1]);
+
+                // show information for one person
+                else if (command == "show" && commandWords.Length > 1)
+                    ShowPerson(commandWords[1]);
+
+                else if (command == "friends" && commandWords.Length > 1)
+                    ShowFriends(commandWords[1]);
+
+                // dump command prints out the graph
+                else if (command == "dump")
+                    rg.Dump();
+
+                // Lists all the orphans in the file, and all orphans have the possibility of being batman
+                else if (command == "orphan")
+                    ShowOrphans();
+                else if (command == "batman")
+                    ShowOrphans();
+
+                // illegal command
+                else
+                    Console.Write("\nLegal commands:\n" + "  read [filename]\n" +
+                        "  dump\n" + "  show [personname]\n" + "  friends [personname]\n" +
+                        "  orphan\n" + "  batman <same as orphan>\n" + "  exit\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLoop();
+        }
+    }
+}
